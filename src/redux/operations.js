3bae807dc1881ca99807efbd32795ce1ae4494bc
Mykola Saliuk <haker0502@ukr.net>
2023-02-25 14:10:24 +0200

    додає операцію addContact

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -1,15 +1,28 @@
-import axios from "axios";
-import { fetchingInProgress, fetchingSuccess, fetcingError } from "./contactsSlice";
+import axios from 'axios';
+import { createAsyncThunk } from '@reduxjs/toolkit';
 
-axios.defaults.baseURL = "https://63f91838083c79ca8b600e8c.mockapi.io"
+axios.defaults.baseURL = 'https://63f91838083c79ca8b600e8c.mockapi.io';
 
-export const fetchContacts = () => async dispatch => {
+export const fetchContacts = createAsyncThunk(
+  'contacts/fetchAll',
+  async (_, thunkAPI) => {
     try {
-        dispatch(fetchingInProgress())
-        const response = await axios.get("/contacts")
-        dispatch(fetchingSuccess(response.data))
+      const response = await axios.get('/contacts');
+      return response.data;
     } catch (error) {
-        dispatch(fetcingError(error.message))
+      return thunkAPI.rejectWithValue(error.message);
     }
-}
+  }
+);
 
+export const addContact = createAsyncThunk(
+  'contacts/addContact',
+  async (newContact, thunkAPI) => {
+    try {
+      const response = await axios.post('/contacts', { newContact });
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
